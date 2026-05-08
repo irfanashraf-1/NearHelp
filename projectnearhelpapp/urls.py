673e58c228831cb12421bffcc4984e6fb1e9a8da
irfanashraf-1 <irfanashrafarm@gmail.com>
2026-05-09 03:26:11 +0530

    2nd commit

--- projectnearhelpapp/urls.py
+++ projectnearhelpapp/urls.py
@@ -3,5 +3,7 @@ from projectnearhelpapp import views
 
 urlpatterns = [
     path('',views.Welcomepage,name='Welcomepage'),
-    path('Job_signup',views.Job_signup,name='Job_signup'),
+    path('Job_signup_page',views.Job_signup_page,name='Job_signup_page'),
+    path('Client_signup_page',views.Client_signup_page,name='Client_signup_page'),
+    path('Login_page',views.Login_page,name='Login_page'),
 ]
--- projectnearhelpapp/views.py
+++ projectnearhelpapp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def Welcomepage(request):
     return render(request,'welcome.html')
-def Job_signup(request):
+def Job_signup_page(request):
     return render(request,'job_signup_form.html')
+def Client_signup_page(request):
+    return render(request,'client_signup_page.html')
+def Login_page(request):
+    return render(request,'login_page.html')
